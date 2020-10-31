8293f49ddd346e2121da8410b28c853c336b2db7
Sam Joyce <samjoycesamjoyce@Sams-MBP.home>
2020-10-31 09:15:45 +0000

    containers added

---
+++ src/containers/Gallery/Gallery.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Gallery.module.scss";
+
+const Gallery = () => {
+  return (
+    <>
+      <p>Gallery works</p>
+    </>
+  );
+};
+
+export default Gallery;
