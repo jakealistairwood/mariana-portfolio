760caaf87982697f099875a5e9e6d2681aabefb3
Sam Joyce <samjoycesamjoyce@Sams-MBP.home>
2020-11-05 19:34:14 +0000

    feature added: when a face is clicked in the gallery the state in routes is set to that student and user is linked back to the main page

--- src/components/Trainee/Trainee.jsx
+++ src/components/Trainee/Trainee.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import styles from "./Trainee.module.scss";
-// import pic from '../../assets/images/Aiste.png';
+import { Link } from "@reach/router";
 
 const Trainee = (props) => {
-  const { name, title, headshot } = props.trainee;
+  const { setTrainee } = props;
+  const { name, title, headshot, index } = props.trainee;
+  // console.log(props.trainee)
   return (
     <>
       <article className={styles.trainee}>
-        <div className={styles.imageContainer}>
-          <img src={headshot} alt="Trainee picture" className={styles.image} />
-          <div className={styles.imageOverlay}></div>
-        </div>
+        <Link to="/">
+          <div className={styles.imageContainer} onClick={() => {
+            console.log(props.trainee)
+            setTrainee(props.trainee)
+          }}>
+            <img
+              src={headshot}
+              alt="Trainee picture"
+              className={styles.image}
+            />
+            <div className={styles.imageOverlay}></div>
+          </div>
+        </Link>
         <h4>{name}</h4>
         <p>{title}</p>
       </article>
--- src/containers/Gallery/Gallery.jsx
+++ src/containers/Gallery/Gallery.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import styles from "./Gallery.module.scss";
 import TraineeList from "../../components/TraineeList";
 
-const Gallery = () => {
+const Gallery = (props) => {
+  const {setTrainee} = props;
   return (
     <>
       <h1 className={styles.gallery_heading}>Meet the Crew.</h1>
       <hr/>
-      <TraineeList />
+      <TraineeList setTrainee={setTrainee} />
     </>
   );
 };
