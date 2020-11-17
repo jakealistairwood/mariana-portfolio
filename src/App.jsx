6f011f7460ea313e21fc824b8dd7c0fd17f294c9
Jake Wood <jakealistairwood@gmail.com>
2020-11-17 17:40:39 +0000

    fixing img on safari - mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import Main from './containers/Main';
 import Footer from './components/Footer';
+// import 'normalize.css';
 
 const App = () => {
   return (
